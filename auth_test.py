b5572931339386b8302ce77d217e5fced3ee05fd
Alex Juma <50864725+AlexJumaW99@users.noreply.github.com>
2025-07-17 17:02:06 -0500

    Delete auth_test.py

--- auth_test.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st 
-
-st.title("Authentication")
-
-if st.button("Authenticate"):
-    st.login("google")
-    # st.session_state.authenticated = True
-    # st.success("You are now logged in!")
